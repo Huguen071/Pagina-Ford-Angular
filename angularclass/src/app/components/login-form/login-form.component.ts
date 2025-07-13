280920b73bcbe088cac1b8db5dc888af7c5fe503
Hugo Colognese <hugocolognese@gmail.com>
2025-07-13 18:00:28 -0300

    login funcionando

--- angularclass/src/app/components/login-form/login-form.component.ts
+++ angularclass/src/app/components/login-form/login-form.component.ts
@@ -1,12 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { LoginService } from '../../service/login.service';
+import { Router } from '@angular/router';
 
 
 @Component({
   selector: 'app-login-form',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './login-form.component.html',
   styleUrl: './login-form.component.css'
 })
 export class LoginFormComponent {
+  loginService = inject(LoginService)
+  router = inject(Router)
 
+  loginForm = new FormGroup({
+    nome: new FormControl("", [Validators.required]),
+    senha: new FormControl("", [Validators.required]),
+  })
+
+  onSubmitLogin() {
+    const { nome, senha } = this.loginForm.value
+
+    if (!this.loginForm.valid || !nome || !senha) {
+      alert("Existem campos não preenchidos!")
+      return
+    }
+
+    this.loginService.login(nome, senha).subscribe({
+      error: (err) =>{
+        if(err.status === 401) {
+          alert ("Usuário ou senha incorretos!")
+        }
+
+          alert ("Erro interno, tente novamente mais tade.")
+      },
+      next: () => {
+        this.router.navigate(["/home"])
+      }
+    })
+  }
 }
