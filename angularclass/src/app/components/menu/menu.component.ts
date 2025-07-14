d348204dcb3d101be6131cdc9e6d34eada2f13a5
Hugo Colognese <hugocolognese@gmail.com>
2025-07-13 21:43:31 -0300

    pagina HOME feita só falta estilizar

--- angularclass/src/app/components/menu/menu.component.ts
+++ angularclass/src/app/components/menu/menu.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-menu',
@@ -7,5 +8,9 @@ import { Component } from '@angular/core';
   styleUrl: './menu.component.css'
 })
 export class MenuComponent {
+  router = inject(Router)
 
+  goToDashboard() {
+    this.router.navigate(["/dashboard"])
+  }
 }
