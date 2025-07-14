d69dccc55d2ce7d5eab04b387ab949e36e6e873e
Hugo Colognese <hugocolognese@gmail.com>
2025-07-13 21:10:59 -0300

    criando a pagina HOME (Botão MENU não funciona)

--- angularclass/src/app/pages/home/home.component.ts
+++ angularclass/src/app/pages/home/home.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { BoasVindasComponent } from "../../components/boas-vindas/boas-vindas.component";
+import { MenuComponent } from '../../components/menu/menu.component';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [BoasVindasComponent, MenuComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
